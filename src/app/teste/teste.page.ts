05eecad3fbb01a788bad12733779c393325bd245
Ferfleuri <fernandofleuri1@gmail.com>
2023-08-10 12:21:01 -0300

    api da grecco

--- src/app/teste/teste.page.ts
+++ src/app/teste/teste.page.ts
@@ -11,7 +11,56 @@ export class TestePage {
 
   constructor() { }
 
+  /**
+   * Obtem dados do API - by Luis Carlos
+   * @returns
+   */
   async ObtemPosicao() {
+    this.result = document.getElementById('txtResultado');
+    let urlAPI = 'http://cliente.dfleet.com.br/data/posicao';
+    const mode : RequestMode = 'no-cors'
+    const settings = {
+      method: 'POST',
+      mode: mode,
+      url: 'http://cliente.dfleet.com.br/data/posicao',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        "Accept": "/",
+        "Connection": "keep-alive",
+        "Content-Length": "0",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
+      },
+      body: JSON.stringify({
+        CHAVE: 'C791E885F7C5F',
+        Placa: '',
+        DataMinima: '',
+        Metodo: 'localizacao'
+      })
+    };
+
+    const response = await fetch(urlAPI, settings);
+    console.log(response)
+    const data = await response.json();
+    console.log(data);
+    // tratamento dos dados
+    /*
+    var resultado = '';
+    for (var x = 0; x < Data.Data.length; x++) {
+      resultado = resultado + 'Rota: ' + Data.Data[x].FROTA + ' - Hora: ' +
+        Data.Data[x].DATAEVENTO.replace('T', ' ') +
+        ' LOCALIZAÇÃO: ' + Data.Data[x].ENDERECO + '<br />';
+    }
+    console.log(resultado);
+    this.result!.innerHTML = resultado;
+    alert('dados recebidos');
+*/
+
+  }
+
+  async ObtemPosicao_ALUNOS() {
+
     var isError = false;
     var msgError = "";
     let CHAVE = 'C791E885F7C5F';
