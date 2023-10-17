5357141a135e842fe9700ac0b9e0143e6472c0cf
livinha1234 <liviamariasouza268@gmail.com>
2023-10-17 08:03:15 -0300

    arrumando

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,14 +1,20 @@
-import { Component, } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LinesService } from '../../services/lines.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
 
-  constructor() {
+export class Tab2Page implements OnInit {
 
+  linhas = [];
+
+  constructor(private linesService: LinesService ) { }
+
+  async ngOnInit() {
+  this.linesService.getLines('horarios').subscribe((res: any) => {this.linhas = (res.content); console.log(this.linhas)})
   }
 
 
