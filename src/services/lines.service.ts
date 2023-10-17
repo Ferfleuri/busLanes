304740c9f9af8de7e9b128088a23ab74aa7a128d
livinha1234 <liviamariasouza268@gmail.com>
2023-10-17 07:43:19 -0300

    fazendo integração

--- src/services/lines.service.ts
+++ src/services/lines.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-
-import {environment} from '../environments'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LinesService {
 
+  httpHeader = {
+    headers: new HttpHeaders ({'Content-type': 'application/json'})
+  };
+
+  url = "http://localhost:8080"
+
   constructor(public http: HttpClient) { }
 
   async getLines() {
-    const response = await this.http.get(`${}`)
+    const response = this.http.get(`${this.url}/linhas/todos`, this.httpHeader)
+    return response.subscribe(
+      response => response,
+    );
   }
 
 }
