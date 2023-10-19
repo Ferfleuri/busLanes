8b51aa46c81808b9f72a44f2bc2aa139d26b9498
Ferfleuri <fernandofleuri1@gmail.com>
2023-10-19 10:49:38 -0300

    a

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -107,8 +107,8 @@ export class Tab2Page implements OnInit {
     this.linha8horario7 = this.linha8.filter((e: any) => e.horarios.semana === "Segunda á Sábado - Fórum")
 
     this.linha10 = this.linhas.filter((e: any) => e.linhas === "Linha 10")
-    this.linha10horario1 = this.linha8.filter((e: any) => e.horarios.semana === "Segunda á Sábado - Fórum")
-    this.linha10horario2 = this.linha8.filter((e: any) => e.horarios.semana === "Segunda á Sábado - Terminal Urbano (centro)")
+    this.linha10horario1 = this.linha10.filter((e: any) => e.horarios.semana === "Segunda á Sábado - Fórum")
+    this.linha10horario2 = this.linha10.filter((e: any) => e.horarios.semana === "Segunda á Sábado - Terminal Urbano (centro)")
 
     this.linha11 = this.linhas.filter((e: any) => e.linhas === "Linha 11")
     this.linha11horario1 = this.linha11.filter((e: any) => e.horarios.semana === "Segunda á Sábado - Terminal Urbano (centro)")
